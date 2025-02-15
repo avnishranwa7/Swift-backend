c713e380fbce400c85e6d40d814e48db71fa831f
avnishranwa9122 <avnish.ranwa9122@softsensor.ai>
2025-02-15 14:33:09 +0530

    setup

---
+++ src/main/java/com/swift/Swift/SwiftApplication.java
@@ -0,0 +1,13 @@
+package com.swift.Swift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swift/Swift/SwiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swift.Swift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
